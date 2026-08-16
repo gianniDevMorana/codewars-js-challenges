c0f3d8a1f0f9fad5bc78d4e0de2e83ff06a84069
Gianni <gianni.dev.morana@gmail.com>
2026-08-16 18:47:50 +0200

    fix

--- 8kyu/square_n_sum.js
+++ 8kyu/square_n_sum.js
@@ -1,9 +1,7 @@
-function squareSum(numbers){
-    let result = 0;
-    for (let i = 0; i < numbers.length; i++) {
-        result += numbers[i] * numbers[i];
-    }
-    return result;
+function squareSum(numbers) {
+  let result = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    result += numbers[i] * numbers[i];
+  }
+  return result;
 }
-
-console.log(squareSum([2, 4]));
