b2fcf8e618d9cbf8ca0d5332f98708d80170e84e
Gianni <gianni.dev.morana@gmail.com>
2026-08-18 01:45:42 +0200

    sum_of_positive

---
+++ 8kyu/sum_of_positive.js
@@ -0,0 +1,10 @@
+function positiveSum(arr) {
+  let sumOfPositive = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sumOfPositive += arr[i];
+    }
+  }
+  return sumOfPositive;
+}
