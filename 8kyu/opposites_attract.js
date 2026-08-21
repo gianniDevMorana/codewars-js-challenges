0a7b19f22ad37cee4d81518f518a354d930f8150
Gianni <gianni.dev.morana@gmail.com>
2026-08-21 08:46:11 +0200

    opposites attract updated code

--- 8kyu/opposites_attract.js
+++ 8kyu/opposites_attract.js
@@ -1,14 +1,6 @@
 function lovefunc(flower1, flower2) {
-  inlove = true;
-  if (
+  return (
     (flower1 % 2 === 0 && flower2 % 2 !== 0) ||
     (flower1 % 2 !== 0 && flower2 % 2 === 0)
-  ) {
-    inlove;
-  } else {
-    inlove = false;
-  }
-  return inlove;
+  );
 }
-
-console.log(lovefunc(3, 3));
