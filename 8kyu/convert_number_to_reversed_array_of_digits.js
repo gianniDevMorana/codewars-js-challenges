bbf2e7c1478ba4b680fca0cb42bc75ffe404370d
Gianni <gianni.dev.morana@gmail.com>
2026-09-20 17:59:49 +0200

    convert number to reversed array of digits

---
+++ 8kyu/convert_number_to_reversed_array_of_digits.js
@@ -0,0 +1,10 @@
+function digitize(n) {
+  const digits = n.toString();
+  let digitsArray = [];
+  for (let i = 0; i < digits.length; i++) {
+    digitsArray += digits[i];
+  }
+  return (digitsArray.split("").reverse()).map(Number);
+}
+
+console.log(digitize(35231));
